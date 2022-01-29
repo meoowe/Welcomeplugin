3fa48fac774386c2f20dfcf18e76c798d1a788f5
bobby29831 <bobby29831@gmail.com>
2022-01-29 10:14:34 -0600

    Swap system to have a fully-configurable message w/placeholders & color codes

--- src/main/java/com/meoowe/welcome/WelcomePlugin.java
+++ src/main/java/com/meoowe/welcome/WelcomePlugin.java
@@ -6,11 +6,18 @@ import com.meoowe.welcome.events.JoinHandler;
 
 public final class WelcomePlugin extends JavaPlugin {
 
+    private static WelcomePlugin instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
         getServer().getPluginManager().registerEvents(new JoinHandler(), this);
         getConfig().options().copyDefaults(true);
         saveDefaultConfig();
     }
 
+    public static WelcomePlugin getInstance() {
+        return instance;
+    }
 }
--- src/main/java/com/meoowe/welcome/events/JoinHandler.java
+++ src/main/java/com/meoowe/welcome/events/JoinHandler.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class JoinHandler implements Listener{
@@ -14,8 +13,10 @@ public class JoinHandler implements Listener{
     @EventHandler
     public static void playerJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
-        String name = JavaPlugin.getPlugin(WelcomePlugin.class).getConfig().getString("Server_name");
-        player.sendMessage(ChatColor.GREEN + "Welcome to " + name + ", "  + player.getName());
+        String message = WelcomePlugin.getInstance().getConfig().getString("join-message");
+        if (message != null) {
+            e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', message.replace("{player}", e.getPlayer().getName())));
+        }
     }
 
 
